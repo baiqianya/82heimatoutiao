d69160b8f23ea8d9202605cd59b0a0c6183fc299
baiqian <779875991@qq.com>
2019-09-04 21:28:00 +0800

    面包屑组件封装完成

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
-
+import breakcrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('break-crumb', breakcrumb)
   }
 }
