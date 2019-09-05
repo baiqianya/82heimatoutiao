e4908422cc8e43f7cbcd759098a32d1b8384354e
baiqian <779875991@qq.com>
2019-09-05 15:24:23 +0800

    评论列表-页面结构-table的使用完成

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
-import breakcrumb from './common/bread-crumb'
+import breakCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
-    Vue.component('break-crumb', breakcrumb)
+    Vue.component('break-crumb', breakCrumb)
   }
 }
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>评论列表</div>
+  <el-card>
+    <break-crumb slot="header">
+      <template slot="title">评论列表</template>
+    </break-crumb>
+    <el-table :data="tableData">
+      <el-table-column label="标题" prop="name" width="650"></el-table-column>
+      <el-table-column label="评论状态" prop="date"></el-table-column>
+      <el-table-column label="总评论数" prop="address"></el-table-column>
+      <el-table-column label="粉丝评论数" prop="name"></el-table-column>
+      <el-table-column label="操作" prop="name"></el-table-column>
+    </el-table>
+  </el-card>
+
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
