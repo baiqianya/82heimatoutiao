5e8ebcf8d7e863f40cc7644b7375e97b9803e9af
baiqian <779875991@qq.com>
2019-09-05 20:10:02 +0800

    评论列表-列表分页及请求完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -13,9 +13,11 @@
           <el-button size="small" type="text">修改</el-button>
           <el-button @click="openOrclose(cz.row)" :style="{color:cz.row.comment_status?'#E6A23C':'#67C23A'}" size="small" type="text">{{cz.row.comment_status?'关闭评论':'打开评论'}}</el-button>
         </template>
-
       </el-table-column>
     </el-table>
+    <el-row type="flex" justify="center" style='margin:10px 0'>
+      <el-pagination  @current-change="changePage" :current-page="page.page" :page-size="page.pageSize" background layout="prev, pager, next" :total="page.total"></el-pagination>
+    </el-row>
   </el-card>
 
 </template>
@@ -24,15 +26,24 @@
 export default {
   data () {
     return {
-      list: []
+      list: [],
+      page: {
+        page: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   methods: {
+    changePage (newPage) {
+      this.page.page = newPage
+      this.getComments()
+    },
     openOrclose (row) {
       let mess = row.comment_status ? '关闭' : '打开'
       this.$confirm(`您是否要${mess}评论?`, '提示').then(() => {
         this.$axios({
-          methods: 'put',
+          method: 'put',
           url: '/comments/status',
           params: { article_id: row.id.toString() },
           data: { allow_comment: !row.comment_status }
@@ -47,9 +58,10 @@ export default {
     getComments () {
       this.$axios({
         url: '/articles',
-        params: { response_type: 'comment' }
+        params: { response_type: 'comment', page: this.page.page, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
         console.log(result)
       })
     }
